852bac80efcb89f29dde722ed12e52b4497320e6
Isabellha <itaborda@unicesar.edu.co>
2026-08-14 12:01:36 -0500

    feat: create Book class extending Material

--- src/com/mycompany/biblioteca/Book.java
+++ src/com/mycompany/biblioteca/Book.java
@@ -1,4 +1,29 @@
 package com.mycompany.biblioteca;
 
-public class Book {
+public class Book extends Material {
+
+    private String author;
+    private boolean available;
+
+    public Book(String code, String title, int publicationYear, String author, boolean available) {
+        super(code, title, publicationYear);
+        this.author = author;
+        this.available = available;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }

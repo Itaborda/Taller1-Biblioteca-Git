8a473eb52b4c1754de2e9f97852351c436312f0a
Isabellha <itaborda@unicesar.edu.co>
2026-08-14 12:01:20 -0500

    feat: create Material parent class

---
+++ src/com/mycompany/biblioteca/Material.java
@@ -0,0 +1,38 @@
+package com.mycompany.biblioteca;
+
+public class Material {
+
+    private String code;
+    private String title;
+    private String publicationYear;
+
+    public Material(String code, String title, int publicationYear) {
+        this.code = code;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
